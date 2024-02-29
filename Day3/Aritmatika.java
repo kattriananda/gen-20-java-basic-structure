478cee3b5cdda9e0e54149529b698e6a5ea31cec
Kattria Nanda Petma <kattriananda@gmail.com>
2024-02-29 10:24:34 +0700

    Membuat Package

--- Day3/Aritmatika.java
+++ Day3/Aritmatika.java
@@ -1,3 +1,5 @@
+package Day3;
+
 import java.util.Scanner;
 
 public class Aritmatika {
@@ -8,17 +10,18 @@ public class Aritmatika {
         System.out.println("==================================");
 
         Scanner input = new Scanner(System.in);
-        int A, B, jumlah, kurang, kali, bagi;
+        int A, B, jumlah, kurang, kali, bagi; //inisialisasi type
 
-        System.out.print("Bilangan 1= ");
+        System.out.print("Bilangan 1= "); //inputan 1 dari user
         A= input.nextInt();
-        System.out.print("Bilangan 2= ");
+        System.out.print("Bilangan 2= "); //inputan 2 dari user
         B= input.nextInt();
 
+        //proses aritmatika
         jumlah= A + B;
         kurang= A-B;
         kali  = A*B;
-        bagi  = A/B;
+        bagi  =  A / B;
 
         System.out.println("Jumlah = "+jumlah);
         System.out.println("Kurang = "+kurang);
--- Day3/Main.java
+++ Day3/Main.java
@@ -1,3 +1,4 @@
+package Day3;
 
 public class Main {
     public static void main(String[] args) {
